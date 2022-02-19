251bc01f67fbe2206c5631a2147eb79918007035
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2022-02-19 02:34:13 +0000

    output to file

--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <cmath>
 #include <time.h>
+#include <fstream>
+#include <cstdlib>
 
 #include "ODE_analysis.h"
 
@@ -29,10 +31,22 @@ int main()
     std::vector<ODEpoint> result_RK4 = pendulum.RungeKutta4(120, 1E-3);
     std::vector<ODEpoint> result_leapfrog = pendulum.LeapFrogImprovedSolver(120, 1E-3);
 
-    pendulum.ODE_Draw("images/euler.png", "time", "#hat{#theta}", 50, result_euler);
-    pendulum.ODE_Draw("images/RK4.png", "time", "#hat{#theta}", 50, result_RK4);
-    pendulum.ODE_Draw("images/leapfrog.png", "time", "#hat{#theta}", 50, result_leapfrog);
+    // pendulum.ODE_Draw("images/euler.png", "time", "#hat{#theta}", 50, result_euler);
+    // pendulum.ODE_Draw("images/RK4.png", "time", "#hat{#theta}", 50, result_RK4);
+    // pendulum.ODE_Draw("images/leapfrog.png", "time", "#hat{#theta}", 50, result_leapfrog);
+    // pendulum.Draw_Stability("images/stability.pdf", 150, 0.0125);
 
-    pendulum.Draw_Stability("images/stability.pdf", 150, 0.0125);
+    std::ofstream outdata;
+    outdata.open("data1.txt"); // opens the file
+    if (!outdata)
+    { // file couldn't be opened
+        std::cerr << "Error: file could not be opened" << std::endl;
+        exit(1);
+    }
+
+    for (int i = 0; i < result_RK4.size(); ++i)
+        outdata << result_RK4[i].T() << "\t" << result_RK4[i].X()[0] << std::endl;
+    outdata.close();
+
     return 0;
 }
