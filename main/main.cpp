60ade7eac246477fef25d5378e1a8d83943cf10d
MiguelLameiras <mlameirasmlameiras@gmail.com>
2022-02-20 22:53:43 +0000

    Ok, acho que era assim que tinhas deixado

--- main/main.cpp
+++ main/main.cpp
@@ -25,29 +25,9 @@ int main()
     pendulum.SetFunction(1, [&](ODEpoint p)
                          { return -((delta + epsilon * std::cos(p.T())) * std::sin((p.X())[0])); });
 
-    // std::vector<ODEpoint> result_euler = pendulum.EulerSolver(120, 0.0125);
-    //std::vector<ODEpoint> result_RK4 = pendulum.RungeKutta4(30, 1E-1);
     std::vector<ODEpoint> result_leapfrog = pendulum.LeapFrogImprovedSolver(30, 1E-1);
 
-    // pendulum.ODE_Draw("images/euler.png", "time", "#hat{#theta}", 50, result_euler);
-    // pendulum.ODE_Draw("images/RK42.png", "time", "#hat{#theta}", 50, result_RK4);
-    // pendulum.ODE_Draw("images/leapfrog.png", "time", "#hat{#theta}", 50, result_leapfrog);
-    // pendulum.Draw_Stability("images/stability.pdf", 150, 0.0125);
-
-    std::ofstream outdata;
-    outdata.open("data1.txt"); // opens the file
-    if (!outdata)
-    { // file couldn't be openedcsv
-        std::cerr << "Error: file could not be opened" << std::endl;
-        exit(1);
-    }
-
-    int n = (int)result_leapfrog.size();
-    for (int i = 0; i < n; ++i)
-    {
-        outdata << result_leapfrog[i].T() << ";" << result_leapfrog[i].X()[0] << std::endl;
-    }
-    outdata.close();
+    WriteData("data1.txt",result_leapfrog);
 
     //Correr o ficheiro python para fazer o dynamic mode decomposition
     std::cout << "\nA correr o código python..." << std::endl;
