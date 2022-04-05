4945677ad37792bc4d9de9536bc59c2d2af822ae
MiguelLameiras <mlameirasmlameiras@gmail.com>
2022-04-05 20:52:59 +0100

    gostoso

--- main/main.cpp
+++ main/main.cpp
@@ -9,10 +9,69 @@
 
 #include "ODE_analysis.h"
 #include "tools.h"
-
+#include "pendulum_parametric.h"
 // ainda nao esta a funcionar :(
 
+void Uma_Ode(); // Função para gerar uma só ode e guardar o output num ficheiro txt
+int Is_Stable(double *, int);
+std::vector<double> Moving_Average(std::vector<ODEpoint>, double, double);
+void Stability_Map(int, double); // Gerar n ODE's, só quero testar umas coisas
+
 int main()
+{
+    // Uma_Ode();
+    Stability_Map(300,0.1);
+
+    return 0;
+}
+
+int Is_Stable(double *media, int n)
+{
+    int flag_estab = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (media[i] > M_PI)
+        {
+            flag_estab = 0;
+            break;
+        }
+    }
+    return flag_estab;
+};
+
+std::vector<double> Moving_Average(std::vector<ODEpoint> vetor, double time_step, double tw)
+{
+    double sum_sq;
+    std::vector<double> amp_mean;
+    amp_mean.resize(vetor.size() - tw);
+    int loop;
+
+    for (int i = 0; i < vetor.size() - tw; i++)
+    {
+        amp_mean[i] = 0;
+    }
+
+    loop = 0;
+    while (loop < tw)
+    {
+        sum_sq += vetor[loop].X()[0] * vetor[loop].X()[0];
+        loop++;
+    }
+    double media = sqrt(sum_sq / tw);
+    amp_mean[0] = media;
+
+    for (int i = 1; i < vetor.size() - tw; i++)
+    {
+        sum_sq += vetor[i + tw].X()[0] * vetor[i + tw].X()[0];
+        sum_sq -= vetor[i - 1].X()[0] * vetor[i - 1].X()[0];
+        media = sqrt(sum_sq / tw);
+        amp_mean[i] = media;
+    }
+
+    return amp_mean;
+};
+
+void Uma_Ode()
 {
     double initial_position = 0.1;
     ODE_analysis pendulum(2, {initial_position, 0});
@@ -62,6 +121,67 @@ int main()
     // Correr o ficheiro python para fazer o dynamic mode decomposition
     std::cout << "\nA correr o código python..." << std::endl;
     system("python3 main/ODEdmd.py");
+}
 
-    return 0;
+void Stability_Map(int numero, double step)
+{
+    int n;
+    int count = 0;
+    
+    //Max and min delta values
+    double delta_range[2] = {0.2, 0.35};
+    double epsilon_range[2] = {0, 0.25};
+
+    double step_delta = (delta_range[1] - delta_range[0]) / numero;
+    double step_epsilon = (epsilon_range[1] - epsilon_range[0]) / numero;
+
+    std::ofstream outdata;
+    outdata.open("main/hist_data.txt"); // opens the file
+    if (!outdata)
+    { // file couldn't be openedcsv
+        std::cerr << "Error: file could not be opened" << std::endl;
+        exit(1);
+    }
+
+    double est = 1;
+
+    std::cout << "A cozinhar esses gráficos... " << std::endl;
+
+    for (double delta = delta_range[0]; delta <= delta_range[1]; delta = delta + step_delta)
+    {
+        for (double epsilon = epsilon_range[0]; epsilon <= epsilon_range[1] + step_epsilon; epsilon = epsilon + step_epsilon)
+        {
+            pendulum_parametric pendulum(1, {0.1, 0});
+            pendulum.SetFunction(0, [](ODEpoint p)
+                                 { return p.X()[1]; });
+
+            pendulum.SetFunction(1, [&](ODEpoint p)
+                                 { return -((delta + epsilon * std::cos(p.T())) * std::sin((p.X())[0])); });
+
+            std::vector<ODEpoint> resultado = pendulum.LeapFrogImprovedSolver(numero, step);
+            std::vector<double> mov_avg = Moving_Average(resultado, step, 5 / step);
+
+            n = mov_avg.size();
+
+            double *media = &mov_avg[0];
+
+            int value = Is_Stable(media, n);
+            if (value == 0)
+                est = 0.1;
+            else
+                est = 1;
+
+            outdata << delta << ";" << epsilon << ";" << est << std::endl;
+        }
+        count++;
+        if (count % 5 == 0)
+            std::cout << "\u001b[32m" << 100 * (delta - delta_range[0]) / (delta_range[1] - delta_range[0]) << "% " << std::endl;
+    }
+    std::cout << " " << std::endl;
+
+    outdata.close();
+
+    // Correr o ficheiro python para fazer o dynamic mode decomposition
+    std::cout << "\n\u001b[37mA correr o código python..." << std::endl;
+    system("python3 main/Hist.py");
 }
